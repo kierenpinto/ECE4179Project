9719a69eecd774d45edd6bfb766cfc3964891888
Kieren Pinto <kieren.pinto@hotmail.com>
2019-10-15 22:38:51 +1100

    Correct dataloader

--- data_load.py
+++ data_load.py
@@ -3,15 +3,21 @@ import os
 import cv2
 from torch.utils.data import Dataset
 import torchvision.transforms as Transforms
+import numpy as np
 
 class dataset(Dataset):
-    def __init__(self, input_folder, target_folder, model,device):
+    def __init__(self, input_folder, target_folder, model,device,validation):
         super(dataset,self).__init__()
         self.input_folder = input_folder
         self.target_folder = target_folder
-
-        self.inputs = [x for x in os.listdir(input_folder)]
-        self.targets = [x for x in os.listdir(target_folder)]
+        if validation:
+            ''' Uses validation set'''
+            self.inputs = os.listdir(input_folder)[5000:]
+            self.targets = os.listdir(target_folder)[5000:]
+        else:
+            ''' uses training set '''
+            self.inputs = os.listdir(input_folder)[0:2000]
+            self.targets = os.listdir(target_folder)[0:2000]
         h, w, c = cv2.imread("{}{}".format(input_folder,self.inputs[0])).shape
         self.model = model
 
@@ -24,7 +30,7 @@ class dataset(Dataset):
         image = self.transform(image)
         target = cv2.imread(self.target_folder+self.targets[index])
         target = cv2.resize(target, (self.resize_shape[2:]))
-        target = self.transform(target)[0]
+        target = np.maximum(target.astype(int)-2,0).transpose(2,0,1)[0]# Convert target from 0,3,4,5 to 0,1,2,3 therefore allowing four classes
         return [image, target]
 
     def __len__(self):
